a7859c4f5e15743d59ba376f3b964f2be43f4aac
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-06 08:49:57 +0500

    Input IP

--- index.js
+++ index.js
@@ -6,5 +6,34 @@ import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+import { exec } from "child_process";
 
-console.log("Hello");
+let ip;
+let localIp;
+
+const getPublicIP = () => {
+  exec("curl ip-adresim.app", function (error, stdout, stderr) {
+    if (error) return;
+    localIp = stdout;
+    askIp();
+  });
+};
+
+const welcome = () => {
+  console.log(chalk.bgBlue(`get-geolocation-details 1.0.0 by Muhammad Uzair`));
+};
+
+const askIp = () => {
+  const ipInput = inquirer.prompt({
+    name: "ipInput",
+    type: "input",
+    prefix: ">",
+    message: `${chalk.greenBright("Enter Any IP Address")}`,
+    default: () => {
+      return localIp;
+    },
+  });
+  ip = ipInput.ipInput;
+};
+welcome();
+getPublicIP();
